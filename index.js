8c6e20fef901b1e1e40baf3bf70241fead57e850
Hepteto <136929258+Hepteto@users.noreply.github.com>
2024-02-07 18:14:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 // config inicial
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const todoRoute = require('./routes/todoRoute');
+const express = require('express')
+const app = express()
 
-dotenv.config();
+const dotenv = require('dotenv')
+dotenv.config()
 
-// Middlewares
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const mongoose = require('mongoose')
+
+const cors = require('cors')
+app.use(cors())
+
+// Forma de ler JSON/ middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
 
 // Rotas da API
-app.use('/todo', todoRoute);
+const todoRoute = require('./routes/todoRoute')
+
+app.use('/todo', todoRoute)
 
 // Rota/ Endpoint inicial
-app.get('/', (req, res) => {
-    res.json({ message: 'Testando API' });
-});
+app.get('/', (req,res) =>{
+    res.json({message: 'Testando API'})
+})
 
-// Conectar ao banco de dados e iniciar o servidor
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log("Conectamos ao mongo DB");
-        const port = process.env.PORT || 3000;
-        app.listen(port, () => {
-            console.log(`Servidor rodando na porta ${port}`);
-        });
-    })
-    .catch((err) => console.error(err));
+
+// entregar uma porta
+mongoose.connect(process.env.MONGO) // conectar ao banco de dados
+.then(() => {
+    console.log("Conectamos ao mongo DB")
+    const port = process.env.PORT || 3000;
+    app.listen(port) // Vai ler a porta
+})
+.catch((err) => console.log(err))
