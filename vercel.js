94b74bd8b3186e844f196916993fc501cfc0daec
Hepteto <136929258+Hepteto@users.noreply.github.com>
2024-02-07 18:15:06 -0300

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,4 +1,23 @@
 {
+  "version":2,
+  "builds": [
+
+    { "src": "*.js", "use": "@vercel/node" }
+  ],
+  "routes": [
+    { "src": "/(.*)", "dest": "/" }
+  ]
+}
+// {
+//   "version": 2,
+//   "builds": [
+//     { "src": "/", "use": "@vercel/node" }
+//   ],
+//   "routes": [
+//     { "src": "/(.*)", "dest": "/" }
+//   ]
+// }
+{
   "version": 2,
   "builds": [
     { "src": "/", "use": "@vercel/node" }
